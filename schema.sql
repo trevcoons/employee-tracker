07709821d2909f887181cb92972702f826926ca2
twizard <trevcoons@gmail.com>
2021-07-27 13:14:23 -0700

    create roles and employees tables

--- schema.sql
+++ schema.sql
@@ -10,3 +10,22 @@ CREATE TABLE department (
     name VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- Create the table for roles
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Create the table for employees
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
